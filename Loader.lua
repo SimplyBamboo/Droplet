bfe9b8039716bff6dddd0d6d9b33fe12aa6ea357
SimplyBamboo <Simply.bamb000@gmail.com>
2025-04-22 03:31:22 -0400

    Add files via upload

---
+++ Loader.lua
@@ -0,0 +1,15 @@
+local url = "https://raw.githubusercontent.com/P-DennyGamingYT/ALCATRAZ-PF/main"
+
+local function import(file)
+    local success, result = pcall(function()
+        return loadstring(game:HttpGet(url .. file))()
+    end)
+
+    if not success then
+        warn('failed to import', file, result) 
+    end
+end
+
+getgenv().import = import
+
+import('/main.lua')
